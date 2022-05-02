4b7c936fb70fd0b0cbbdec5ea4adc7007a2f726d
Myrto92 <98779941+Myrto92@users.noreply.github.com>
2022-05-02 15:55:37 +0300

    added photos

--- src/Form.js
+++ src/Form.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import "./Form.css";
 import axios from "axios";
+import Photos from "./Photos.js";
 import Results from "./Results.js";
 
 export default function Form() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
+  }
 
-    console.log(response.data[0].meanings[0].definitions[0].definition);
+  function HandlePixelsResponse(response) {
+    console.log(response.data.photos);
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -19,6 +24,11 @@ export default function Form() {
     let api = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 
     axios.get(api).then(handleResponse);
+
+    let pixelsKey = "563492ad6f91700001000001ec31a84d03f743e3be0c40f58e689f08";
+    let pixelsUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    let headers = { Authorization: `Bearer ${pixelsKey}` };
+    axios.get(pixelsUrl, { headers: headers }).then(HandlePixelsResponse);
   }
 
   function handleChange(event) {
@@ -40,6 +50,7 @@ export default function Form() {
       </form>
 
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
